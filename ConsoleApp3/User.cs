2372c311fc93ec5ce189e320632d59d6333d8b1b
Liz Schilleci <elizabethschilleci@gmail.com>
2017-10-09 11:04:49 -0700

    Initial adding of files

---
+++ ConsoleApp3/User.cs
@@ -0,0 +1,17 @@
+﻿namespace OnboardingApp
+{
+	public class User
+	{
+		internal readonly string FullName;
+
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public bool IsAccountOwner { get; set; }
+		public string PinNumber { get; set; }
+
+
+		public User()
+		{
+		}
+	}
+}
